9292d7fdfd753b3654801f5460d43a2c4c65bce9
AitorPT <aitor.pelaez.tomas@gmail.com>
2024-08-28 12:04:35 +0200

    Add files via upload

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;  
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int count = 5000000;
+        HashSet<int> numbers = new HashSet<int>();
+        Random random = new Random();
+        string filePath = "numeros_random2.txt";
+
+        Stopwatch stopwatch = new Stopwatch(); 
+        stopwatch.Start(); 
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            while (numbers.Count < count)
+            {
+                int num = random.Next(int.MinValue, int.MaxValue);
+                if (numbers.Add(num))
+                {
+                    writer.WriteLine(num);
+                }
+            }
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Tiempo de ejecución: {stopwatch.ElapsedMilliseconds / 1000.0} segundos");
+        Console.WriteLine("Bien.");
+    }
+}
